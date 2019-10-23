98989b79ab277ec51a6cd5d469dfe9f29669f5fd
Albert <lliuguangbo@126.com>
2019-10-23 17:54:04 +0800

    更新kotandroid lib

---
+++ kotandroid/src/main/java/com/sovegetables/ContextExtension.kt
@@ -0,0 +1,71 @@
+package com.sovegetables
+
+import android.annotation.SuppressLint
+import android.app.ActivityManager
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import androidx.annotation.DrawableRes
+
+fun Context.toast(msg: String, long:Int = Toast.LENGTH_SHORT){
+    ToastHelper.toast(this, msg, long)
+}
+
+fun Context.toUri(@DrawableRes drawableId:Int): Uri {
+    return Uri.parse(
+        ContentResolver.SCHEME_ANDROID_RESOURCE +
+                "://" + this.resources.getResourcePackageName(drawableId)
+                + '/'.toString() + this.resources.getResourceTypeName(drawableId)
+                + '/'.toString() + this.resources.getResourceEntryName(drawableId)
+    )
+}
+
+fun Context.toStringUrl(@DrawableRes drawableId:Int): String {
+    return (ContentResolver.SCHEME_ANDROID_RESOURCE +
+            "://" + this.resources.getResourcePackageName(drawableId)
+            + '/'.toString() + this.resources.getResourceTypeName(drawableId)
+            + '/'.toString() + this.resources.getResourceEntryName(drawableId))
+}
+
+fun Context.getProcessName(): String? {
+    val am = this.getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+    var processInfos: List<ActivityManager.RunningAppProcessInfo>? = am.runningAppProcesses
+    if (processInfos == null) {
+        processInfos = am.runningAppProcesses
+    }
+    if (processInfos != null) {
+        for (run in processInfos) {
+            if (run.pid == android.os.Process.myPid()) {
+                return run.processName
+            }
+        }
+    }
+    return null
+}
+
+
+@SuppressLint("ShowToast")
+object ToastHelper {
+    var mToast: Toast? = null
+    fun toast(con: Context?, resId: Int,duration: Int = Toast.LENGTH_SHORT) {
+        if (con == null) {
+            return
+        }
+        toast(con, con.getString(resId), duration)
+    }
+
+    fun toast(con: Context?, text: String?, duration: Int = Toast.LENGTH_SHORT) {
+        if (con == null || text == null) {
+            return
+        }
+        val context = con.applicationContext
+        if (mToast == null) {
+            mToast = Toast.makeText(context.applicationContext, text, Toast.LENGTH_LONG)
+        } else {
+            mToast!!.setText(text)
+            mToast!!.duration = duration
+        }
+        mToast!!.show()
+    }
+}
---
+++ kotandroid/src/main/java/com/sovegetables/VersionExtension.kt
@@ -0,0 +1,43 @@
+package com.sovegetables
+
+import android.os.Build
+
+fun afterApi26(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+}
+
+fun afterApi25(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1
+}
+
+fun afterApi24(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N
+}
+
+fun afterApi23(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+}
+
+fun afterApi21(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP
+}
+
+fun afterApi20(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT_WATCH
+}
+
+fun afterApi19(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT
+}
+
+fun afterApi18(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2
+}
+
+fun afterApi17(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1
+}
+
+fun afterApi16(): Boolean {
+    return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+}
